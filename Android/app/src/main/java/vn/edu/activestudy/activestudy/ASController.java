0e14ecadb7560fcb771e4d3e7b502586f99e3def
vuthanhtrung90 <trungvt.bk@gmail.com>
2015-08-17 21:01:19 +0700

    create Input Email and OTP screen

---
+++ Android/app/src/main/java/vn/edu/activestudy/activestudy/ASController.java
@@ -0,0 +1,15 @@
+package vn.edu.activestudy.activestudy;
+
+/**
+ * Created by dell123 on 8/17/2015.
+ */
+public class ASController {
+
+    private ASController mInstance;
+
+    public ASController getInstance() {
+        return mInstance;
+    }
+
+
+}
