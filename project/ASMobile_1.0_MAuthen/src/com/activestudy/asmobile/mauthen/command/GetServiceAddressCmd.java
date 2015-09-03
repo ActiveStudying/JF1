03970f94df46807eb13d4e3e40f7149f4d8c4434
phamvanoanh <oanh@oanh-Vostro-3460>
2015-09-03 15:59:07 +0700

    getServiceAddress

--- project/ASMobile_1.0_MAuthen/src/com/activestudy/asmobile/mauthen/command/GetServiceAddressCmd.java
+++ project/ASMobile_1.0_MAuthen/src/com/activestudy/asmobile/mauthen/command/GetServiceAddressCmd.java
@@ -4,7 +4,22 @@ package com.activestudy.asmobile.mauthen.command;
  *
  * @author PC
  */
-public class GetServiceAddressCmd {
+public class GetServiceAddressCmd extends ASBaseCommand {
+
+    String sessionId;
+
     
-    
+
+    @Override
+    public void execute() {
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
 }
